0ef77f0a3e9dff0b41ae8f65282bb0438a173e3d
arunadas <aruna.das29@gmail.com>
2023-07-15 23:33:15 -0700

    numbers

---
+++ chapter-2/numbers.py
@@ -0,0 +1,42 @@
+print(0.2 + 0.1)
+print(4/2)
+
+universe_age = 14_000_000_000
+print(universe_age)
+
+#multiple assignment
+x , y , z = 0 , 0 , 0
+
+#constant
+MAX_CONNECTIONS = 5000
+
+print(5+3)
+print(10-2)
+print(4*2)
+print(16//2)
+
+favorite_number = 9
+print(f"My fav number is {favorite_number}")
+
+""" import this
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
