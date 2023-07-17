ffafbc05c2831deafb0c5ef409b543711d702c23
arunadas <aruna.das29@gmail.com>
2023-07-16 21:58:39 -0700

    list assignment

---
+++ chapter-3/bicycles.py
@@ -0,0 +1,14 @@
+bicycles  = ['trek', 'cannondale', 'redline', 'speacialized']
+
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+
+print(bicycles[1])
+print(bicycles[3])
+
+# last element
+print(bicycles[-1])
+
+message = f"My first bicycle was s {bicycles[0].title()}"
+print(message)
