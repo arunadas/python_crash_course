dd1fc485408655e9b2a3ab3775c4aff2205f98cf
arunadas <aruna.das29@gmail.com>
2023-07-18 13:27:09 -0700

    List function

---
+++ chapter-3/greetings.py
@@ -0,0 +1,7 @@
+friends = ['Shandiz', 'John', 'Sumedha', 'Jyotsna']
+
+message = f"My good friend {friends[0].title()}"
+print(message)
+
+message = f"{friends[-1]} is always nice to me"
+print(message)
