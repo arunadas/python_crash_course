51c78240266827077ecef202d7ddd4616626daaf
arunadas <aruna.das29@gmail.com>
2023-07-19 15:40:30 -0700

    List function

---
+++ chapter-3/guests_change.py
@@ -0,0 +1,8 @@
+guests = ['picaso', 'trovolta','therman']
+
+print(f"{guests[0].title()} can\'t make to the dinner")
+guests[0] = 'nicole'
+
+print(f"{guests[0].title()} , you are invited to dinner this saturday!")
+print(f"{guests[1].title()} , you are invited to dinner this saturday!")
+print(f"{guests[2].title()} , you are invited to dinner this saturday!")
