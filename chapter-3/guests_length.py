50745ff32eb7bcd06ff45bccd5190109a65e7a25
arunadas <aruna.das29@gmail.com>
2023-07-20 10:39:06 -0700

    List function

---
+++ chapter-3/guests_length.py
@@ -0,0 +1,9 @@
+guests = ['picaso', 'trovolta','therman']
+
+print(f"{guests[0].title()} can\'t make to the dinner")
+guests[0] = 'nicole'
+guests.insert(0,'vivekananda')
+guests.insert(3,'arbindo')
+guests.append('subash')
+
+print(f"length of guests {len(guests)}")
