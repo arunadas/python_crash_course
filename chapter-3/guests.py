bd0668ca8b932a662481ee67aae9e4442b720332
arunadas <aruna.das29@gmail.com>
2023-07-21 18:49:28 -0700

    List function

---
+++ chapter-3/guests.py
@@ -0,0 +1,4 @@
+guests = ['picaso', 'trovolta','therman']
+print(f"{guests[0].title()} , you are invited to dinner this saturday!")
+print(f"{guests[1].title()} , you are invited to dinner this saturday!")
+print(f"{guests[2].title()} , you are invited to dinner this saturday!")
