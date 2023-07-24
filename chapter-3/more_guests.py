b279635013f54b067000c63bef43e654d5dccc7f
arunadas <aruna.das29@gmail.com>
2023-07-23 22:02:42 -0700

    List function

---
+++ chapter-3/more_guests.py
@@ -0,0 +1,14 @@
+guests = ['picaso', 'trovolta','therman']
+
+print(f"{guests[0].title()} can\'t make to the dinner")
+guests[0] = 'nicole'
+guests.insert(0,'vivekananda')
+guests.insert(3,'arbindo')
+guests.append('subash')
+
+print(f"{guests[0].title()} , you are invited to dinner this saturday!")
+print(f"{guests[1].title()} , you are invited to dinner this saturday!")
+print(f"{guests[2].title()} , you are invited to dinner this saturday!")
+print(f"{guests[3].title()} , you are invited to dinner this saturday!")
+print(f"{guests[4].title()} , you are invited to dinner this saturday!")
+print(f"{guests[5].title()} , you are invited to dinner this saturday!")
