7b1386c89f4fb2ac167e5fca35885b953c2c2863
arunadas <aruna.das29@gmail.com>
2023-07-28 13:07:11 -0700

    List function

---
+++ chapter-3/transportations.py
@@ -0,0 +1,6 @@
+my_favorite = ['car', 'enfield', 'airplane']
+message = f"I would like to own {my_favorite[1].title()}"
+print(message)
+
+message = f"I want to gift my friend {my_favorite[2].title()}"
+print(message)
