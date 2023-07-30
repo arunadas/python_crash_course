c1dc4030112f8500ad2119cfe1eefc3de491fcb9
arunadas <aruna.das29@gmail.com>
2023-07-30 13:40:53 -0700

    List function

---
+++ chapter-4/animals.py
@@ -0,0 +1,9 @@
+animals = ['dog', 'cat','squirel']
+
+for animal in animals:
+    print(animal)
+
+for animal in animals:
+    print(f"{animal.title()} will make a great pet")
+
+print(f"Any of these animals would make good pet!")        
