f416cfc144147b3086c70501397abfacf5aa37a2
arunadas <aruna.das29@gmail.com>
2023-07-31 10:58:59 -0700

    List function

---
+++ chapter-4/buffet.py
@@ -0,0 +1,13 @@
+buffet = ('naan','butter chicken','ghee roast','mutton biriyani','ladoo')
+
+print('Original buffet')
+for food in buffet:
+    print(food)
+
+#buffet[3] = 'icecream'  
+
+buffet =  ('naan','butter chicken','lava cake','icecream','ladoo')
+
+print('\nNew buffet')
+for food in buffet:
+    print(food)
