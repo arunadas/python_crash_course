8386e0a0fbb5e724aa21f1f33f2bab52cdb06529
arunadas <aruna.das29@gmail.com>
2023-08-10 10:03:07 -0700

    list function

---
+++ chapter-4/numbers.py
@@ -0,0 +1,14 @@
+for number in range(1,21):
+    print(number)
+
+for number in range(1,20,2):
+    print(number)    
+
+for number in range(3,31,3):
+    print(number)
+
+for value in range(1,11):
+    print(value ** 3)    
+
+cubes = [value ** 3 for value in range(1,11)]
+print(cubes)    
