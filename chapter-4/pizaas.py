7686275b26c4d885064ae6bc1ae97a26c5482694
arunadas <aruna.das29@gmail.com>
2023-08-11 21:12:49 -0700

    list function

---
+++ chapter-4/pizaas.py
@@ -0,0 +1,8 @@
+pizzas = ['peporoni','mozrella','deepCrust']
+for pizza in pizaas:
+    print(pizza)
+
+for pizza in pizaas:
+    print(f"I like {pizza.title()} pizza")
+
+print(f"I really love pizza!")    
