2fa84a0fb001b328aaa4fff658fa4606735f1ba3
arunadas <aruna.das29@gmail.com>
2023-08-13 11:44:11 -0700

    list function

---
+++ chapter-4/pizzas.py
@@ -0,0 +1,11 @@
+pizzas = ['peporoni','mozrella','deepCrust']
+friend_pizzas = pizzas[:]
+
+pizzas.append('agnipath')
+friend_pizzas.append('new york')
+
+print("My favorite pizzas")
+print(pizzas)
+
+print("\nMy friends Favorite pizzas")
+print(friend_pizzas)
