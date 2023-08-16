eb66e0660d64649bc1bee59d8ba0dec8b90c3129
arunadas <aruna.das29@gmail.com>
2023-08-15 21:19:38 -0700

    list function

---
+++ chapter-4/slices.py
@@ -0,0 +1,7 @@
+animals = ['dog', 'cat','squirel', 'shark','lion','duck']
+print("The first three items in the list are:")
+print(animals[:3])
+print("Three items from the middle of the list are:")
+print(animals[3:])
+print("The last three element in the list are :")
+print(animals[-3:])
