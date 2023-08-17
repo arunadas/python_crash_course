43a332005139d5bca3dd4dd2b18e26b19d6ea145
arunadas <aruna.das29@gmail.com>
2023-08-16 22:15:37 -0700

    list function

---
+++ chapter-4/square_numbers.py
@@ -0,0 +1,17 @@
+squares = []
+for value in range(1,11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)  
+
+squares2 = []
+for value in range(1,11):
+    squares2.append(value ** 2)
+
+print(squares2)
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
