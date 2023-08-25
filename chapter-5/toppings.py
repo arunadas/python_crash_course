f162bca4a5ff4c69816fa44447a19d7a65ec276a
arunadas <aruna.das29@gmail.com>
2023-08-25 15:31:45 -0700

    condition check

---
+++ chapter-5/toppings.py
@@ -0,0 +1,7 @@
+requested_toppings = ['mushrooms','extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushroom")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese")        
