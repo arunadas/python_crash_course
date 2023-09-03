5f86cba1911c55ca16d3ed787327102987b280de
arunadas <aruna.das29@gmail.com>
2023-09-02 20:39:48 -0700

    for loop

---
+++ chapter-5/solution/alien_colors3.py
@@ -0,0 +1,10 @@
+alien_colors = ['green' , 'yellow','red']
+
+for alien_color in alien_colors:
+    if alien_color == 'green':
+        print("player earned 5 points")
+    elif alien_color =='yellow':
+        print("player earned 10 points")
+    elif alien_color == 'red':
+        print("player earned 15 points")        
+   
