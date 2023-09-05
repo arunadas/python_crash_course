6f7672e26f97442c39fa7a20c012211b84808500
arunadas <aruna.das29@gmail.com>
2023-09-05 14:33:52 -0700

    condtonal testsi

---
+++ chapter-5/solution/conditional_tests.py
@@ -0,0 +1,39 @@
+fruit = 'banana'
+
+if fruit == 'banana':
+    print(" Today fruit is banana")
+else:
+    print(" Its not banana")    
+
+car = 'tesla'
+if car == 'tesla':
+    print("its an electric car")
+else:
+    print("its not an electric car")
+
+flower = 'Rose'
+if flower.lower() == 'rose':
+    print("its rose garden")
+else:
+    print("its not rose garden") 
+
+temp = 90
+
+if temp > 65:
+    print("its hot today")
+else:
+    print("it not so hot today")  
+
+age = 19
+if age >= 18 :
+    print("you can vote")
+else:
+    print("you can't vote") 
+
+fruit = ['banana','grape']
+
+if 'grape' in fruit:
+    print("Yes")
+
+if 'strawberry' not in fruit:
+    print('No')    
