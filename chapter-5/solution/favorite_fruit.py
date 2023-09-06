7d618390b7d67bc50b6a95db335e5a89a9f7cd3e
arunadas <aruna.das29@gmail.com>
2023-09-06 10:40:04 -0700

    conditional check

---
+++ chapter-5/solution/favorite_fruit.py
@@ -0,0 +1,13 @@
+favourite_fruits = ['grapes', 'strawberry', 'blue berries']
+
+if 'blue berries' in favourite_fruits:
+    print("I really like blue berries")
+if 'apple' in favourite_fruits:
+    print("I dont like apple")
+if 'strawberry' in favourite_fruits:
+    print("I really like strawberry")
+if 'grapes' in favourite_fruits:
+    print("I really like grapes")
+if 'banana' in favourite_fruits:
+    print("I don't like banana")
+           
