38dc2827f7f7b00b8723071a3023a9080cca1613
arunadas <aruna.das29@gmail.com>
2023-09-26 10:40:38 -0700

    dictionary user

---
+++ chapter-6/solution/person.py
@@ -0,0 +1,11 @@
+person_0 = {
+    'first_name' : 'jhonny',
+    'last_name' : 'lee',
+    'age' : 35 ,
+    'city' : 'sanfrancisco'
+}
+
+print(person_0['first_name'])
+print(person_0['last_name'])
+print(person_0['age'])
+print(person_0['city'])
