4065d63f3e13d52111b36e1fcf22a882ebbcbc8a
arunadas <aruna.das29@gmail.com>
2023-09-25 20:09:48 -0700

    dictionary user

---
+++ chapter-6/solution/glossary.py
@@ -0,0 +1,10 @@
+glossary  = {
+    'iterate' : 'loop through element',
+    'list' : ' collection of ordered hetrogenous items',
+    'dictionary' : 'key , value pair',
+    'conditional check' : 'if , elif , else',
+    'set' : 'collectoin of unique values'
+}
+
+for k , v in glossary.items():
+    print(k, ":", v)
