5dc15269dffee123e568432ad726c3678d9d4ce9
arunadas <aruna.das29@gmail.com>
2023-10-13 09:59:04 -0700

    assignment solution

---
+++ chapter-7/solution/multiple_of_ten.py
@@ -0,0 +1,7 @@
+number = input("Give me a number I will tell you whether its multiple of ten. ")
+number = int(number)
+
+if number % 10 == 0:
+    print("Number id multiple of ten. ")
+else:
+    print("Number is not multiple of ten. ")   
