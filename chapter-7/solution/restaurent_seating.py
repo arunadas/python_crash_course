b8c928d5c4736afa14659170fa06cc37e59aead0
arunadas <aruna.das29@gmail.com>
2023-10-17 15:35:05 -0700

    assignment solution

---
+++ chapter-7/solution/restaurent_seating.py
@@ -0,0 +1,7 @@
+seats = input("How many people are there in your dinning group ? ")
+seats = int(seats)
+
+if seats > 8 :
+    print("You will have to wait")
+else:
+    print("Your table is ready")    
