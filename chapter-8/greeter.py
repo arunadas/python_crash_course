dc79682d3a2d5a6618fcc43564a2d2f1af8fa12a
arunadas <aruna.das29@gmail.com>
2023-10-20 18:33:16 -0700

    chapter 8 examples

---
+++ chapter-8/greeter.py
@@ -0,0 +1,12 @@
+def greet_user():
+    """Display a simple greeting. """
+    print("Hello!")
+
+greet_user()    
+
+
+def greet_user2(username):
+    """Display a simple greeting. """
+    print(f"Hello! , {username.title()}!")
+
+greet_user2('jesse')    
