75dbcea9fdfac8f4839112b1841dc68f0b3318e3
arunadas <aruna.das29@gmail.com>
2023-11-11 19:52:19 -0800

    chapter 8 solution

---
+++ chapter-8/solution/describe_city.py
@@ -0,0 +1,7 @@
+def describe_city(name, country='India'):
+    """Describe the city"""
+    print(f"{name} is in {country}")
+
+describe_city('varanasi')
+describe_city('Mathura')
+describe_city('Fremont', "USA")    
