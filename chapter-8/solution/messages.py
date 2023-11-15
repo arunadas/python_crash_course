29ce4caea399d7c91b4441924985cdf2a1f5ad76
arunadas <aruna.das29@gmail.com>
2023-11-14 19:08:32 -0800

    chapter 8 solution

---
+++ chapter-8/solution/messages.py
@@ -0,0 +1,9 @@
+def show_messages(messages):
+    """Print all the messages"""
+    for message in messages:
+        print(message)
+
+
+
+messages = ['how are you', 'hello','hows the wheather']  
+show_messages(messages)  
