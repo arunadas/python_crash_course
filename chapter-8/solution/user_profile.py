c76ce9cc4f0b97dda89ecc1b186a726b9ab180b0
arunadas <aruna.das29@gmail.com>
2023-11-14 19:13:05 -0800

    chapter 8 solution

---
+++ chapter-8/solution/user_profile.py
@@ -0,0 +1,10 @@
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing"""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('aruna', 'das', 
+                             location='california',
+                             field='engineering')
+print(user_profile)
