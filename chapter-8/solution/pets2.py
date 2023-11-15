47f61e47aa39475f4a5186af3e60769c8cdcd4b7
arunadas <aruna.das29@gmail.com>
2023-11-14 19:09:54 -0800

    chapter 8 solution

---
+++ chapter-8/solution/pets2.py
@@ -0,0 +1,6 @@
+from pets_functions import describe_pet
+
+describe_pet('hamster', 'harry')    
+describe_pet('dog', 'willie')
+describe_pet(pet_name='Simba', animal_type='lion')
+describe_pet(pet_name='Pinku')
