ebe12eec6f5406490238f0b006dcb033114e4d73
arunadas <aruna.das29@gmail.com>
2023-11-14 19:08:10 -0800

    chapter 8 solution

---
+++ chapter-8/solution/make_shirt.py
@@ -0,0 +1,18 @@
+def make_shirt(size='large', message= 'I love California'):
+    """Summarize the shirt that's going to be made"""
+    print(f"\n I am going to making T shirt size of {size}")
+    print(f"It will say {message}")
+
+make_shirt()
+make_shirt('medium')
+make_shirt('small', 'I like fremont')   
+
+
+def make_shirt(size='large', message= 'I love Python'):
+    """Summarize the shirt that's going to be made"""
+    print(f"\n I am going to making T shirt size of {size}")
+    print(f"It will say {message}")
+
+make_shirt()
+make_shirt('medium')
+make_shirt('small', 'I like fremont') 
