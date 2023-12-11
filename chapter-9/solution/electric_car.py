fec76d51fd9f56fcba906de05440e22ff186b446
arunadas <aruna.das29@gmail.com>
2023-12-10 20:37:21 -0800

    solutions of chp 9

---
+++ chapter-9/solution/electric_car.py
@@ -0,0 +1,29 @@
+from car_only import Car
+class Battery:
+    """Model battery for electric car."""
+
+    def __init__(self, battery_size = 40):
+        """Initialize the battery's attributes """
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kwh battery.")   
+
+    def get_range(self):
+        "What is the range of this battery"
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")         
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery = Battery() 
