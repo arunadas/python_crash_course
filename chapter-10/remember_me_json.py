b11936d6a6617c1ab40acc180f56b51ebfc9df35
arunadas <aruna.das29@gmail.com>
2024-01-23 19:25:42 -0800

    examples of cht 10

---
+++ chapter-10/remember_me_json.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import json
+
+def get_stored_username(path):
+    """Get stored username if available"""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+
+def get_new_username(path):
+    """Prompt for a new username""" 
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents) 
+    return username
+
+
+def greet_user():
+    """Greet the user bby name"""
+    path = Path('/Users/prakash/Desktop/Aruna-learning/linkedin/python/python_crash_course/chapter-10/username.json')
+    username = get_stored_username(path)
+    if username:
+        print(f"Welcome back, {username}")
+    else:
+        username = get_new_username(path)
+        print(f"We will remember you when you will come back, {username}!")
+
+greet_user()
