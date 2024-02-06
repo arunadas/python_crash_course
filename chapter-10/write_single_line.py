603d6913a4dd9b9dc2afbb88eab27a1da14321c3
arunadas <aruna.das29@gmail.com>
2024-02-05 19:33:09 -0800

    examples of cht 10

---
+++ chapter-10/write_single_line.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+path = Path('/Users/prakash/Desktop/Aruna-learning/linkedin/python/python_crash_course/chapter-10/single_file.txt')
+path.write_text("I love programming")
