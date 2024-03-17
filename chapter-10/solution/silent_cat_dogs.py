b988c1a7f57ec455486c6db82136e5b17721dfa6
arunadas <aruna.das29@gmail.com>
2024-03-17 13:50:17 -0700

    examples of cht 10

---
+++ chapter-10/solution/silent_cat_dogs.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+filenames = ['cats.txt', 'dog.txt']
+
+def read_files(filename):
+    path = Path(filename)
+    return path.read_text()
+
+for filename in filenames:
+
+    try:
+        contents = read_files(f"/Users/prakash/Desktop/Aruna-learning/linkedin/python/python_crash_course/chapter-10/solution/{filename}")
+    except FileNotFoundError:
+        pass
+    else:
+        print(contents)
+
+
+
---
+++ chapter-10/solution/simpler_code.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+path = Path('/Users/prakash/Desktop/Aruna-learning/linkedin/python/python_crash_course/chapter-10/solution/learning_python.txt')
+
+contents = path.read_text().strip()
+for line in contents.splitlines():
+    print(line)
